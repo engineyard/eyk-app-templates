8fe94f285603d180ae7fadd27a7151a16f370e0a
dbroemme <darren.broemmer@gmail.com>
2021-04-11 12:51:23 -0400

    Minor cleanup

--- test/system/poll_system_test.rb
+++ test/system/poll_system_test.rb
@@ -2,13 +2,6 @@ require "application_system_test_case"
 
 class PollsTest < ApplicationSystemTestCase
     include Devise::Test::IntegrationHelpers 
-   
-    #test "view the poll" do
-    #    sign_in users(:yahoo)
-
-    #    visit '/poll/index'
-    #    assert_selector "h3", text: "What is your favorite weekend activity"
-    #end
 
     test "answer the poll" do
         sign_in users(:yahoo)
