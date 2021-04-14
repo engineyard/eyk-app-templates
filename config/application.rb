eeab994047e8e12ff65fadab8d4fa75409ccd3cb
dbroemme <darren.broemmer@gmail.com>
2021-04-14 18:15:24 -0400

    Upgrade to Rails 6

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,22 +11,14 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    config.active_job.queue_adapter = :sidekiq 
-
-    # AppMap
+    # Configuration for the application, engines, and railties goes here.
     #
-    # application.rb is a good place to do this, along with all the other railties.
-    # Don't require the railtie in environments that don't bundle the appmap gem.
-    require 'appmap/railtie' if defined?(AppMap)
-    config.appmap.enabled = true if ENV['APPMAP_RECORD']
-
-    # Rollout
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
     #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
     unless Rails.env.production?
       # Mock redis in non-production environments
       $redis = MockRedis.new
@@ -38,5 +30,6 @@ module Blog
     Rollout::UI.configure do
       instance { $rollout }
     end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
-require 'sidekiq/web'
-require 'sidekiq-scheduler/web'
-
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root 'poll#index'
   get 'poll/index'
@@ -11,7 +9,6 @@ Rails.application.routes.draw do
 
   post 'poll/reward'
 
-  mount Sidekiq::Web => '/sidekiq'
   mount Blazer::Engine, at: "blazer"
-  mount Rollout::UI::Web.new => '/admin/rollout'
+  mount Rollout::UI::Web.new => '/admin/rollout'  
 end
